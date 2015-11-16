797f2ff9ad96a4a8ee681159a7466b83961ee93c
Manewu101 <Manewu@gmx.net>
2015-11-16 22:11:09 +0100

    I2C Block functions, ok.

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ var piIO = require('./build/Release/bcm2835-io');
 
 console.log(piIO.initIO());
 console.log(piIO.setGPIO(12, 'out'));
-console.log(piIO.writeGPIO(12, false));
+console.log(piIO.writeGPIO(12, !piIO.readGPIO(12)));
 console.log("In value: ", piIO.readGPIO(12));
 console.log("SetGPIO_pud: ", piIO.setGPIO_pud(21, 2));
+
 console.log("beginI2c: ", piIO.beginI2c());
-console.log("writeI2c: ", piIO.writeI2c(new Buffer([10, 20]), 2));
+buf = new Buffer(32*4);
+
+//gesture sensor
+console.log("setI2cSlaveAddress: ", piIO.setI2cSlaveAddress(0x39));
+console.log("writeI2c: ", piIO.writeI2c(new Buffer([0x80, 0x7F]), 2));
+
+//piIO.writeI2c(new Buffer([0xE7]), 1);   //I2C "address accessing to clear interrupt
+console.log("readRegisterI2c: ", piIO.readRegisterRsI2c(0x80, buf, 32*4));
+console.log("writeReadRsI2c: ", piIO.writeReadRsI2c(new Buffer([0x80]), 1, buf, 32*4));
+
+console.log("buf: ", buf);
+console.log("endI2c: ", piIO.endI2c());
--- src/bcm2835-io.cc
+++ src/bcm2835-io.cc
@@ -58,6 +58,73 @@ void WriteI2c(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 #endif
 }
 
+void ReadI2c(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+#ifdef __arm__
+    if (info.Length() != 2) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
+    if (!node::Buffer::HasInstance(info[0]) || !info[1]->IsNumber()) {
+        Nan::ThrowTypeError("First argument should be Buffer, second byte #");
+        return;
+    }
+    char* buf = (char*) node::Buffer::Data(info[0]->ToObject());
+    uint32_t len = info[1]->Uint32Value();
+
+    //uint8_t bcm2835_i2c_read(char* buf, uint32_t len)
+    uint8_t value = bcm2835_i2c_read(buf, len);
+
+    info.GetReturnValue().Set(value);   //bcm2835I2CReasonCodes, 0 = Success
+#else
+#endif
+}
+
+void ReadRegisterRsI2c(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+#ifdef __arm__
+    if (info.Length() != 3) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
+    if (!info[0]->IsNumber() || !node::Buffer::HasInstance(info[1]) || !info[2]->IsNumber()) {
+        Nan::ThrowTypeError("First argument should be command/addr., second Buffer, third byte #");
+        return;
+    }
+    char* buf = (char*) node::Buffer::Data(info[1]->ToObject());
+    uint32_t len = info[2]->Uint32Value();
+    char cmd = info[0]->NumberValue();
+
+    //uint8_t bcm2835_i2c_read_register_rs(char* regaddr, char* buf, uint32_t len)
+    uint8_t value = bcm2835_i2c_read_register_rs(&cmd, buf, len);
+
+    info.GetReturnValue().Set(value);   //bcm2835I2CReasonCodes, 0 = Success
+#else
+#endif
+}
+
+void WriteReadRsI2c(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+#ifdef __arm__
+    if (info.Length() != 4) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
+    if (!node::Buffer::HasInstance(info[0]) || !info[1]->IsNumber() ||
+            !node::Buffer::HasInstance(info[2]) || !info[3]->IsNumber()) {
+        Nan::ThrowTypeError("First argument should be cmds buffer, second byte #, third Buffer, fourth byte #");
+        return;
+    }
+    char* cmds = (char*) node::Buffer::Data(info[0]->ToObject());
+    char* buf = (char*) node::Buffer::Data(info[2]->ToObject());
+    uint32_t len_cmds = info[1]->Uint32Value();
+    uint32_t len_buf = info[3]->Uint32Value();
+
+    //uint8_t bcm2835_i2c_write_read_rs(char* cmds, uint32_t cmds_len, char* buf, uint32_t buf_len)
+    uint8_t value = bcm2835_i2c_write_read_rs(cmds, len_cmds, buf, len_buf);
+
+    info.GetReturnValue().Set(value);   //bcm2835I2CReasonCodes, 0 = Success
+#else
+#endif
+}
+
 void SetGPIO_pud(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 #ifdef __arm__
     if (info.Length() < 2) {
@@ -173,8 +240,14 @@ void Init(v8::Local<v8::Object> exports) {
                  Nan::New<v8::FunctionTemplate>(EndI2c)->GetFunction());
     exports->Set(Nan::New("setI2cSlaveAddress").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(SetI2cSlaveAddress)->GetFunction());
+    exports->Set(Nan::New("readI2c").ToLocalChecked(),
+                 Nan::New<v8::FunctionTemplate>(ReadI2c)->GetFunction());
     exports->Set(Nan::New("writeI2c").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(WriteI2c)->GetFunction());
+    exports->Set(Nan::New("readRegisterRsI2c").ToLocalChecked(),
+                 Nan::New<v8::FunctionTemplate>(ReadRegisterRsI2c)->GetFunction());
+    exports->Set(Nan::New("writeReadRsI2c").ToLocalChecked(),
+                 Nan::New<v8::FunctionTemplate>(WriteReadRsI2c)->GetFunction());
     exports->Set(Nan::New("setGPIO_pud").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(SetGPIO_pud)->GetFunction());
     exports->Set(Nan::New("setGPIO").ToLocalChecked(),
